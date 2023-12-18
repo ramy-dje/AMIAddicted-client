ca03032227501947502c90de89883769c74ee7c6
ramy-dje <ramyromirso@gmail.com>
2023-12-18 17:00:23 +0100

    Initial commit

---
+++ src/welcome/components/Business.jsx
@@ -0,0 +1,52 @@
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="star" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23.4px] mb-1 ">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px]">
+        {content}
+      </p>
+    </div>
+  </div>
+);
+
+const Business = () =>  (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+      be emotionally good <br className="sm:block hidden" />with our experts
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      An online gaming addiction clinic offers specialized counseling and interventions
+       to help individuals overcome gaming-related challenges. Through virtual sessions,
+        trained therapists provide personalized strategies, fostering a healthier balance
+         between gaming and overall well-being, conveniently accessible from home.
+
+
+
+
+
+
+
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
+    </div>
+  </section>
+);
+
+export default Business;
---
+++ src/welcome/constants/index.js
@@ -0,0 +1,197 @@
+import {map,time, people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+  {
+    id: "get-account",
+    title: "Get account",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: map,
+    title: "Geographical Flexibility",
+    content:
+      "Remote therapy allows individuals to access therapy services from anywhere, overcoming geographical barriers",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Anonymous and Confidential",
+    content:
+      "Clients may feel more comfortable discussing sensitive topics online, and platforms prioritize confidentiality and privacy to create a secure therapeutic environment.",
+  },
+  {
+    id: "feature-3",
+    icon: time,
+    title: "Flexibility in Scheduling",
+    content:
+      "Online therapy platforms often provide extended hours, making it easier for clients to find appointments that fit into their schedules",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "The gaming addiction program changed my life. Tailored counseling provided a roadmap to break free. Today, I celebrate a healthier balance, renewed relationships, and a future filled with genuine joy.",
+    name: "Herman Jensen",
+    title: "Client",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "The online gaming addiction clinic became my lifeline. Expert guidance empowered me to regain control. Now, I enjoy a more fulfilling life, with improved focus, stronger connections, and a renewed sense of self",
+    name: "Steve Mark",
+    title: "Client",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Finding this clinic was a game-changer. Personalized strategies gave me the tools to reclaim my life. Grateful for the transformative journey, I now embrace a balanced lifestyle, free from the chains of excessive gaming.",
+    name: "Kenn Gallagher",
+    title: "Client",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Clients",
+    value: "1900+",
+  },
+  {
+    id: "stats-2",
+    title: "therapi Experts",
+    value: "200+",
+  },
+  {
+    id: "stats-3",
+    title: "happy clients",
+    value: "95%",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
