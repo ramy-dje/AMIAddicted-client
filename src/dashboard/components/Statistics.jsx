bdaef3993a7bb59c204bd6f1be21960a1981244b
ramy-dje <ramyromirso@gmail.com>
2023-12-28 18:38:46 +0100

    link front end with backend

--- src/dashboard/components/Statistics.jsx
+++ src/dashboard/components/Statistics.jsx
@@ -1,46 +1,42 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import {admin,doctor,myPatients} from '../assets/assets'
 import {XAxis,YAxis,Bar,AreaChart,ResponsiveContainer,Label,LabelList,Tooltip, Area,BarChart} from 'recharts';
+import axios from 'axios'
 
 const Statistics = () => {
-    const analyticsData = [
-        {name:'Jun 2023',uv: 3},
-        {name:'July 2023',uv: 2},
-        {name:'Augut 2023',uv: 5},
-        {name:'Sept 2023',uv: 7},
-        {name:'Oct 2023',uv: 3},
-        {name:'Nov 2023',uv: 4},
-        {name:'Dicem 2023',uv: 7},
-        {name:'Jun 2023',uv: 3},
-        {name:'July 2023',uv: 2},
-        {name:'Augut 2023',uv: 5},
-        {name:'Sept 2023',uv: 7},
-        {name:'Oct 2023',uv: 3},
-        {name:'Nov 2023',uv: 4},
-        {name:'Dicem 2023',uv: 7},
-      ]
+    const [analyticsData, setanalyticsData] = useState([]);
+    const [doctorsNumber, setdoctorsNumber] = useState(0);
+    const [patientNumber, setpatientNumber] = useState(0);
+    const [adminsNumber, setadminsNumber] = useState(0);
+    useEffect(()=>{
+        axios.get('http://localhost:3000/api/getAnalytics').then((res)=>{setanalyticsData(res.data);console.log(res.data)})
+        axios.get('http://localhost:3000/api/getDoctors').then((res)=>setdoctorsNumber(res.data.length))
+        axios.get('http://localhost:3000/api/getPatients').then((res)=>setpatientNumber(res.data.length))
+        axios.get('http://localhost:3000/api/getAdmins').then((res)=>setadminsNumber(res.data.length))
+    },[])
+  
       let minValue= 0 ;
   return (
     <div className='w-full h-screen p-4 pb-16 flex flex-col  overflow-y-auto' >
         <div className='flex gap-4 items-center justify-center flex-wrap   '>
-            <StatisticsCard img={admin} thisMonth={2} title={'admin'} total={6}/>
-            <StatisticsCard img={doctor} thisMonth={5} title={'doctor'} total={12}/>
-            <StatisticsCard img={myPatients} thisMonth={15} title={'client'} total={30}/>
+            <StatisticsCard img={admin} thisMonth={2} title={'admin'} total={adminsNumber}/>
+            <StatisticsCard img={doctor} thisMonth={5} title={'doctor'} total={doctorsNumber}/>
+            <StatisticsCard img={myPatients} thisMonth={15} title={'client'} total={patientNumber}/>
         </div>
         <div className='w-full mt-5 flex gap-4 h-fit flex-wrap '>
             <div className=' h-[300px] p-3  w-full sm:w-[60%]  bg-[#171825] custom-shadow rounded-lg'>
                 <h1 className='text-white sm:text-xl text-lg pb-3'>Subscriptions in the last 12 months</h1>
-                <ResponsiveContainer width='100%' height='100%' >
-                    <BarChart width={50} height={100} data={analyticsData}>
-                        <XAxis dataKey='name'>
+                {analyticsData && <ResponsiveContainer width='100%' height='100%' >
+                    <BarChart width={150} height={300} data={analyticsData}>
+                        <XAxis dataKey='month'>
                         <Label offset={0} position='insideBottom'/>
                         </XAxis>
                         <YAxis domain={[minValue,'auto']}/>
-                        <Bar dataKey='uv' fill='white'>
+                        <Bar dataKey='count' fill='white'>
                         <LabelList dataKey='uv' position='top'/>
                         </Bar>
                     </BarChart>
-                </ResponsiveContainer>
+                </ResponsiveContainer>}
             </div>
             <div className='md:w-[35%] w-full  bg-[#171825] custom-shadow rounded-lg text-white p-3'>
                 <h1 className='sm:text-xl text-lg pb-3'>Admins List</h1>
